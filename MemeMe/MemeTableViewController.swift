6c8ea1184c1d3ad3989ddf4ea92ff190ad6d5cf5
Ahmed Alhamazani <ahmed@Ahmeds-MacBook-Pro.local>
2019-04-14 22:25:09 +0300

    Add MemeTableViewController

---
+++ MemeMe/MemeTableViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MemeTableViewController.swift
+//  MemeMe
+//
+//  Created by Ahmed Alhamazani on 14/04/2019.
+//  Copyright © 2019 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class MemeTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "rowReuseIdentifier")!
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
