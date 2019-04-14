6e7db13dbf3abab77762f15ea15bbc7f370c17eb
Ahmed Alhamazani <ahmed@Ahmeds-MacBook-Pro.local>
2019-04-14 22:32:52 +0300

    Add MemeCollectionViewController

---
+++ MemeMe/MemeCollectionViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MemeCollectionViewController.swift
+//  MemeMe
+//
+//  Created by Ahmed Alhamazani on 14/04/2019.
+//  Copyright © 2019 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "itemReuseIdentifier", for: indexPath)
+        return cell
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
