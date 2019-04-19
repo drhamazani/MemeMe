f15c7c5bb5840cff751796b078183f086d1f8ca0
Ahmed Alhamazani <ahmed@Ahmeds-MacBook-Pro.local>
2019-04-19 15:44:05 +0300

    Add MemeTableViewCell

---
+++ MemeMe/MemeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MemeTableViewCell.swift
+//  MemeMe
+//
+//  Created by Ahmed Alhamazani on 19/04/2019.
+//  Copyright © 2019 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class MemeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
